5484a0896b3833f9dc69914739e6b30b8e99fe9a
Mohammad <mohammad.m@live.ca>
2018-05-26 13:06:01 -0400

    Updated containers.py to include Queue, Bucket, & ContainerFullException

--- containers.py
+++ containers.py
@@ -1,6 +1,10 @@
-class EmptyContainerError(Exception):
+class ContainerEmptyException(Exception):
     def __str__(self):
         return "The container you are trying to pop from is empty"
+
+class ContainerFullException(Exception):
+    def __str__(self):
+        return "The container you are trying to push into is full"
 
 
 class Container():
@@ -24,4 +28,29 @@ class Stack(Container):
             item = self._container.pop(-1)
         except:
             raise EmptyContainerError
+        return item
+
+
+class Queue(Container):
+
+    def pop(self):
+        try:
+            item = self._container.pop(0)
+        except:
+            raise ContainerEmptyException
+        return item
+
+class Bucket(Container):
+
+    def push(self, item):
+        if len(self._container) == 1:
+            raise ContainerFullException
+        else:
+            self._container = [item]
+
+    def pop(self):
+        try:
+            item = self._container.pop(0)
+        except:
+            raise ContainerEmptyException
         return item
